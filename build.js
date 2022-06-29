088c3c58fa60ee5faf57673fab38f5d52256574c
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2022-06-28 20:20:19 -0700

    build: add css support

--- build.js
+++ build.js
@@ -1,12 +1,13 @@
 require("esbuild")
   .build({
     entryPoints: ["src/index.tsx"],
-    outfile: "build/js/bundle.js",
+    outfile: "build/bundle.js",
     minify: true,
     bundle: true,
     sourcemap: true,
     target: ["chrome58", "firefox57", "edge16"],
     loader: { ".ts": "ts" },
+    watch: true,
   })
   .then(() => console.log("⚡ Done"))
   .catch(() => process.exit(1));
--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <h1>Hello App!</h1>;
+  return <h1 style={{ textAlign: "center" }}>ESBuild React Example!</h1>;
 };
 
 export default App;
